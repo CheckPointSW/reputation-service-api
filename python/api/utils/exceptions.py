845bbcf406a01a2939c84267dfcb84a0a5296638
Aviad Levy <59531105+chkp-aviadl@users.noreply.github.com>
2020-03-03 11:18:30 +0200

    Use old string format to support older versions of python 3 (#7)

--- python/api/utils/exceptions.py
+++ python/api/utils/exceptions.py
@@ -1,10 +1,7 @@
-import sys
-
-
 class StatusCodeException(Exception):
     def __init__(self, status_code: int, *args: object) -> None:
         super().__init__(*args)
         self.status_code = status_code
 
     def __str__(self) -> str:
-        return f"Operation failed with status {self.status_code}"
+        return "Operation failed with status {}".format(self.status_code)
